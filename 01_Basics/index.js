65b2a4dd6e1d2482c990b85c5a84c9d69490ed19
Harsha P <109081765+harshaparashuram@users.noreply.github.com>
2024-08-26 08:28:12 +0000

    added AccountInfo

--- 01_Basics/index.js
+++ 01_Basics/index.js
@@ -1,1 +1,9 @@
-console.log("Hello Harsha");
+// Account information
+
+const accountId = 13071999;
+let accountName = "Harsha";
+var accountPassword = "LeaveMeAl1,.!";
+accountCity = "Davanagere";
+
+console.log(accountId);
+console.table([accountId, accountName, accountPassword, accountCity])
